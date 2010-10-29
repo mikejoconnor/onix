d81df3abaa159444f1b5cbb4a4d9c4ffc7e170bf
mikejoconnor <mike@commerceguys.com>
2010-10-29 12:53:10 -0400

    Initial commit of the onix module, starting with onix_product_identifier

---
+++ modules/onix_product_identifier/theme/onix-product-identifier-default-formatter.tpl.php
@@ -0,0 +1,5 @@
+<ProductIdentifier>
+  <ProductIDType><?php print $identifier['onix_product_identifier_type'];?></ProductIDType>
+  <IDTypeName><?php print $identifier['onix_product_identifier_type_name'];?></IDTypeName>
+  <IDValue><?php print $identifier['onix_product_identifier_value'];?></IDValue>
+</ProductIdentifier>
